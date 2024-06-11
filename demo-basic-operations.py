38f6b6ae9a4dcf8b92c85a4f422d4749bccb68ca
sgrcgfdd <1203494336@qq.com>
2024-06-11 14:37:20 +0800

    Update demo-basic-operations.py

--- demo-basic-operations.py
+++ demo-basic-operations.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Hello, 2208!")
+st.title("放假!")
 st.header("You need to learn some basic operations")
 st.subheader("eg, Visual Studio Code")
 st.text("It's not that hard")
